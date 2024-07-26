7d6928d3911e005848ed1ec6f62c2c6fe3710a12
korallll <kirbor2014@yandex.ru>
2024-07-27 00:15:32 +0300

    inititalization working

--- learners/momentum_learner.py
+++ learners/momentum_learner.py
@@ -6,7 +6,7 @@ import torch
 class MomentumLearner(TrainLearner):
     def __init__(self, model, dls, loss_func, lr, cbs, opt_func=optim.SGD, mom=0.85): 
         self.mom = mom
-        super().__init__(model, dls, loss_func, lr, cbs, opt_func=optim.SGD)
+        super().__init__(model, dls, loss_func, lr, cbs, opt_func)
 
     def zero_grad(self):
         with torch.no_grad():
