7817e353b419fb99b53d06ced09530309cc798f4
korallll <kirbor2014@yandex.ru>
2024-08-04 18:34:36 +0300

    hooks

--- callbacks/hooks_callback.py
+++ callbacks/hooks_callback.py
@@ -9,18 +9,18 @@ from utils import Hooks, append_stats
 
 
 class HooksCallback(Callback):
-    def __init__(self, hookfunc, mod_filter=fc.noop, on_train=True, on_valid=False, mods=None):
+    def __init__(self, hookfunc, mod_filter=fc.noop):
         fc.store_attr()
         super().__init__()
 
-    def before_fit(self, learn):
-        if self.mods: mods = self.mods
-        else: mods = fc.filter_ex(learn.model.modules(), self.mod_filter)
-        self.hooks = Hooks(mods, partial(self._hookfunc, learn))
+    def before_fit(self):
+        mods = fc.filter_ex(self.learn.model.modules(), self.mod_filter)
+        self.hooks = Hooks(mods, self._hookfunc)
 
-    def _hookfunc(self, learn, *args, **kwargs):
-        if (self.on_train and learn.training) or (self.on_valid and not learn.training): self.hookfunc(*args, **kwargs)
+    def _hookfunc(self, *args, **kwargs):
+        if self.learn.model.training:
+            self.hookfunc(*args, **kwargs)
 
-    def after_fit(self, learn): self.hooks.remove()
+    def after_fit(self): self.hooks.remove()
     def __iter__(self): return iter(self.hooks)
     def __len__(self): return len(self.hooks)
