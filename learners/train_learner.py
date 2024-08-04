c3fcb28f3ca41ac10ea219d4c65f6bd2c54daac4
korallll <kirbor2014@yandex.ru>
2024-08-04 18:37:53 +0300

    trainleear"

--- learners/train_learner.py
+++ learners/train_learner.py
@@ -1,8 +1,9 @@
-from .learner import *
+from .learner import Learner
+
 
 class TrainLearner(Learner):
     def predict(self): self.preds = self.model(self.batch[0])
     def get_loss(self): self.loss = self.loss_func(self.preds, self.batch[1])
     def backward(self): self.loss.backward()
     def step(self): self.opt.step()
-    def zero_grad(self): selt.opt.zero_grad()
+    def zero_grad(self): self.opt.zero_grad()
